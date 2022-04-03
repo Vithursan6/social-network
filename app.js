7d93ebb09008e113fbdf4486c70848c2bd094c46
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-04-03 19:24:20 -0400

    added express-jwt, web token auth, added required auth

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
 const dotenv = require("dotenv");
 dotenv.config();
 
-// db
-// MONGO_URI=mongodb://localhost/nodeapi
+
+//local: MONGO_URI=mongodb://localhost/nodeapi
 mongoose
     .connect(
         process.env.MONGO_URI,
-        { useNewUrlParser: true },
-        { useUnifiedTopology: true}
+        { useNewUrlParser: true }
     )
     .then(() => console.log("DB Connected"));
 
@@ -21,16 +21,24 @@ mongoose.connection.on("error", err => {
     console.log(`DB connection error: ${err.message}`);
 });
 
-// bring in routes
+
 const postRoutes = require("./routes/post");
+const authRoutes = require("./routes/auth");
 
 // middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(expressValidator());
 app.use("/", postRoutes);
+app.use("/", authRoutes);
+app.use(function(err, req, res, next) {
+    if(err.name === "UnauthorizedError") {
+        res.status(401).json({ error: "Unauthorized User!"});
+    }
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    console.log(`A Node Js API is listening on port: ${port}`);
+    console.log(`Connected: Listening on port: ${port}`);
 });
